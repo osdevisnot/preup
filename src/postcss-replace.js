ccdb5e66aa1b5a0483937275e8674cc856d3973b
osdevisnot <osdevisnot@gmail.com>
2018-02-13 11:43:14 -0800

    add custom replacements on generated dist css files + allow flexible commit message for release

---
+++ src/postcss-replace.js
@@ -0,0 +1,21 @@
+var postcss = require('postcss')
+var parser = require('postcss-value-parser')
+
+module.exports = postcss.plugin('postcss-replace', () => {
+  return function(css) {
+    css.walkDecls(function(decl) {
+      var root = parser(decl.value)
+
+      root.nodes = root.nodes.map(function(node) {
+        const value = node.value
+        if (node.type === 'function' && node.value === 'url') {
+          node.nodes.map(function(unode) {
+            unode.value = unode.value.replace('../../dist/', '')
+          })
+        }
+        return node
+      })
+      decl.value = root.toString()
+    })
+  }
+})
