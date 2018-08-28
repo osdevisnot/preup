f5ab4c9094b0b1da7e63d76f8161252e152107e3
osdevisnot <osdevisnot@gmail.com>
2018-08-28 10:24:43 -0700

    add experimental multi entry support

--- src/rollup.config.js
+++ src/rollup.config.js
@@ -19,6 +19,9 @@ const plugins = require('./postcss')
 const pkg = require(path.join(process.cwd(), './package.json'))
 const external = Object.keys(Object.assign({}, pkg.dependencies, pkg.devDependencies, pkg.peerDependencies))
 const libraryName = pkg.name.split('/').pop()
+const exportName = libraryName.replace(/-([a-z])/g, function(g) {
+  return g[1].toUpperCase()
+})
 
 /**
  * Options for HTML minifier
@@ -31,48 +34,76 @@ const htmlMinifierOptions = {
 
 const tsHack = pkg.preup.compiler !== 'babel'
 
-export default {
+const rollupPlugins = [
+  // Replacements to make TS `import * as ...` work with babel
+  $.conditional(tsHack, [$.replace({ '*': 'import', delimiters: ['import ', ' as'] })]),
+  // Replacements to consider NODE_ENV optimizations
+  $.replace({ 'process.env.NODE_ENV': JSON.stringify('PRODUCTION') }),
+  // minify HTML
+  $.html({ htmlMinifierOptions }),
+  // inline JSON
+  $.json({ preferConst: true }),
+  // inline images :(
+  $.image(),
+  // inline everything else :(
+  $.url({
+    limit: 10 * 1024,
+    include: ['**/*.woff', '**/*.woff2']
+  }),
+  $.postcss({
+    extract: `dist/${libraryName}.css`,
+    plugins
+  }),
+  $.babel(
+    babelrc({
+      addExternalHelpersPlugin: false,
+      externalHelpers: true,
+      config: babel,
+      exclude: 'node_modules/**',
+      addModuleOptions: false,
+      babelrc: false
+    })
+  ),
+  $.commonjs(),
+  $.cleanup({ comments: 'none' }),
+  $.license({ banner }),
+  $.filesize()
+]
+
+let config = {
   input: pkg.preup.src,
   external,
-  output: {
-    name: libraryName,
-    file: `dist/${libraryName}.js`,
-    format: 'cjs',
-    exports: 'named'
-  },
-  plugins: [
-    // Replacements to make TS `import * as ...` work with babel
-    $.conditional(tsHack, [$.replace({ '*': 'import', delimiters: ['import ', ' as'] })]),
-    // Replacements to consider NODE_ENV optimizations
-    $.replace({ 'process.env.NODE_ENV': JSON.stringify('PRODUCTION') }),
-    // minify HTML
-    $.html({ htmlMinifierOptions }),
-    // inline JSON
-    $.json({ preferConst: true }),
-    // inline images :(
-    $.image(),
-    // inline everything else :(
-    $.url({
-      limit: 10 * 1024,
-      include: ['**/*.woff', '**/*.woff2']
-    }),
-    $.postcss({
-      extract: `dist/${libraryName}.css`,
-      plugins
-    }),
-    $.babel(
-      babelrc({
-        addExternalHelpersPlugin: false,
-        externalHelpers: true,
-        config: babel,
-        exclude: 'node_modules/**',
-        addModuleOptions: false,
-        babelrc: false
-      })
-    ),
-    $.commonjs(),
-    $.cleanup({ comments: 'none' }),
-    $.license({ banner }),
-    $.filesize()
-  ]
+  output: [
+    {
+      name: exportName,
+      file: `dist/${libraryName}.js`,
+      format: 'cjs',
+      exports: 'named'
+    },
+    { name: exportName, file: `dist/${libraryName}.umd.js`, format: 'umd' },
+    { name: exportName, file: `dist/${libraryName}.es5.js`, format: 'iife' }
+  ],
+  plugins: rollupPlugins
 }
+
+if (pkg.preup.multi) {
+  config = [config]
+  Object.keys(pkg.preup.multi).forEach(entry => {
+    const gloablName = entry.replace(/-([a-z])/g, function(g) {
+      return g[1].toUpperCase()
+    })
+    config.push({
+      input: pkg.preup.multi[entry],
+      external,
+      output: {
+        file: `dist/${entry}.es5.js`,
+        format: 'iife',
+        name: gloablName,
+        globals: gloablName
+      },
+      plugins: rollupPlugins
+    })
+  })
+}
+
+export default config
